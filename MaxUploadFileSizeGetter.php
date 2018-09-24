76d241592e707c68f790fd0f4a6a6df514d1304c
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2018-09-24 22:17:27 +0200

    Initial commit

---
+++ MaxUploadFileSizeGetter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SubbySnake\MaxUploadFileSize;
+
+class MaxUploadFileSizeGetter {
+    private $units = 'bkmgtpezy';
+
+    /**
+     * @param null|string $unit
+     * @return string
+     */
+    public function get($unit = null) {
+        $maxSize = 0;
+
+        $postMaxSize = $this->parseSize(ini_get('post_max_size'));
+        $uploadMaxSize = $this->parseSize(ini_get('upload_max_filesize'));
+
+        if($uploadMaxSize > 0 && ($uploadMaxSize <= $postMaxSize || $postMaxSize == 0)) {
+            $maxSize = $uploadMaxSize;
+        } elseif($postMaxSize > 0 && ($postMaxSize <= $uploadMaxSize || $uploadMaxSize = 0)) {
+            $maxSize = $postMaxSize;
+        }
+
+        if($maxSize == 0) {
+            return 'unlimited';
+        }
+
+        if($unit) {
+            $maxSize = floor($maxSize / pow(1024, stripos($this->units, $unit[0])));
+        } else {
+            $i = 0;
+            while($maxSize > 1024) {
+                $maxSize /= 1024;
+                $i++;
+            }
+            $maxSize = floor($maxSize);
+            $unit = strtoupper(str_replace('bb', 'b', $this->units[$i] . 'b'));
+        }
+
+        return number_format($maxSize, 0, ',', ' ') . " {$unit}";
+    }
+
+    private function parseSize($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size);
+        $size = preg_replace('/[^0-9\.]/', '', $size);
+
+        if($unit) {
+            return floor($size * pow(1024, stripos($this->units, $unit[0])));
+        } else {
+            return floor($size);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# MaxUploadFileSize
+
+> Gets maximum of upload file size.
+
+## Installation
+
+Open a command console, enter your project directory and execute the
+following command to download the latest stable version of this bundle:
+
+```console
+$ composer require subbysnake/max-upload-file-size
+```
+
+This command requires you to have Composer installed globally, as explained
+in the [installation chapter](https://getcomposer.org/doc/00-intro.md)
+of the Composer documentation.
+
+## Usage
+
+```php
+$hash = \SubbySnake\MaxUploadFileSize\MaxUploadFileSizeGetter::get();	    // max upload file size rounded (down) with automatic unit
+$hash = \SubbySnake\MaxUploadFileSize\MaxUploadFileSizeGetter::get('KB');	// max upload file size rounded (down) in KB
+```
